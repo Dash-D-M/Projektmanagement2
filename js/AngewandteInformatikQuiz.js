8ff5c6e339d02052ec7e4ac42b57871a1e82dc16
Damon Mittag <dmittag.business@gmail.com>
2025-08-18 12:17:06 +0200

    Antworten zu Quizzes hinhugefügt

--- js/AngewandteInformatikQuiz.js
+++ js/AngewandteInformatikQuiz.js
@@ -1,7 +1,14 @@
 import { initQuiz } from './quizBase.js';
 
 const correctAnswers = {
-  // TODO: Add correct answers for this quiz
+  question1: 'a',
+  question2: 'd',
+  question3: 'b',
+  question4: ['rough animator', 'roughanimator'],
+  question5: 'b',
+  question6: ['flexibilität', 'flexibel'],
+  question7: 'd',
+  question8: 'b'
 };
 
 document.addEventListener('DOMContentLoaded', () => {
