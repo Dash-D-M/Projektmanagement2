d9b832311c4a0a186a2228fa6cec5f65715ab96e
Dash-D-M <dmittag.business@gmail.com>
2025-08-18 12:14:09 +0200

    Add retry modal for incomplete quiz results

---
+++ js/MedieninformatikQuiz.js
@@ -0,0 +1,9 @@
+import { initQuiz } from './quizBase.js';
+
+const correctAnswers = {
+  // TODO: Add correct answers for this quiz
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  initQuiz('quiz-form', correctAnswers);
+});
