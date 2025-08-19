230c0752a67c44d671eb8caeb010fd3b7c73dd07
Damon Mittag <dmittag.business@gmail.com>
2025-08-19 10:42:43 +0200

    Fragen angepasst

--- js/GrundlagenGestaltungQuiz.js
+++ js/GrundlagenGestaltungQuiz.js
@@ -2,13 +2,9 @@ import { initQuiz } from './quizBase.js';
 
 const correctAnswers = {
   question1: 'a',
-  question2: 'd',
-  question3: 'b',
-  question4: ['rough animator', 'roughanimator'],
-  question5: 'b',
-  question6: ['flexibilität', 'flexibel'],
-  question7: 'd',
-  question8: 'b'
+  question2: ['flexibilität', 'flexibel'],
+  question3: 'd',
+  question4: 'b'
 };
 
 document.addEventListener('DOMContentLoaded', () => {
