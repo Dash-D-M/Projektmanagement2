afa4933a0bf261d80df0af49d3ccf8b4bf67d4e0
Damon Mittag <dmittag.business@gmail.com>
2025-08-19 13:18:50 +0200

    Quizfragen überarbeitet

--- js/GrundlagenGestaltungQuiz.js
+++ js/GrundlagenGestaltungQuiz.js
@@ -1,7 +1,7 @@
 import { initQuiz } from './quizBase.js';
 
 const correctAnswers = {
-  question1: 'a',
+  question1: 'd',
   question2: ['flexibilität', 'flexibel'],
   question3: 'd',
   question4: 'b'
--- js/Web-TechnologienQuiz.js
+++ js/Web-TechnologienQuiz.js
@@ -2,7 +2,7 @@ import { initQuiz } from './quizBase.js';
 
 const correctAnswers = {
   question1: 'a',
-  question2: 'd',
+  question2: 'a',
   question3: 'b',
   question4: ['rough animator', 'roughanimator'],
   question5: 'b',
